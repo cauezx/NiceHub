d847d1ffac2abbd7a48fbb78cdcbeb46cc682b45
cauezx <malvado123u@gmail.com>
2025-06-11 06:31:13 -0300

    Update kaitun.lua

--- scripts/kaitun.lua
+++ scripts/kaitun.lua
@@ -1,3 +1,4 @@
 -- Exemplo simples: envia uma mensagem para o jogador local
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
+local config = getgenv().ConfigsKaitun or {}
